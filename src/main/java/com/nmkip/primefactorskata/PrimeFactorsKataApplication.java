22a4d9dfb61a02478c708394ad5eac1d52daeed7
nmkip <juan.ignacio848@gmail.com>
2018-09-18 16:34:29 -0300

    first commit

---
+++ src/main/java/com/nmkip/primefactorskata/PrimeFactorsKataApplication.java
@@ -0,0 +1,12 @@
+package com.nmkip.primefactorskata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeFactorsKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeFactorsKataApplication.class, args);
+    }
+}
