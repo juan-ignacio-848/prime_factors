dbb873571abea48f1c07c65db65ed0ed4006d1b4
nmkip <juan.ignacio848@gmail.com>
2018-09-18 16:59:21 -0300

    We handle the case where n = 2.

--- src/main/java/com/nmkip/primefactorskata/PrimeFactors.java
+++ src/main/java/com/nmkip/primefactorskata/PrimeFactors.java
@@ -1,12 +1,18 @@
 package com.nmkip.primefactorskata;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
 
     public static List<Integer> of(int n) {
-        return Collections.emptyList();
+
+        final List<Integer> factors = new ArrayList<>();
+        if(n == 2) {
+            factors.add(2);
+        }
+
+        return factors;
     }
 
 }
--- src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
+++ src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
@@ -3,13 +3,21 @@ package com.nmkip.primefactorskata;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PrimeFactorsTest {
 
     @Test
     public void of_1_isEmptyList() {
         assertThat(PrimeFactors.of(1), is(Lists.emptyList()));
+    }
+
+    @Test
+    public void of_2_is2() {
+        assertThat(PrimeFactors.of(2), is(Arrays.asList(2)));
     }
 }
