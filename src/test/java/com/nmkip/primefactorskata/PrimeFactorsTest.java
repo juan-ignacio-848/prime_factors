d1905c4fc68701962dca87b59e95c4dc2c71c1af
nmkip <juan.ignacio848@gmail.com>
2018-09-22 17:01:29 -0300

    Prime factors of 2 returns [2]. Removed duplication of tests by adding a setUp method.

--- src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
+++ src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
@@ -1,16 +1,30 @@
 package com.nmkip.primefactorskata;
 
 import org.assertj.core.util.Lists;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PrimeFactorsTest {
 
+    private PrimeFactors primeFactors;
+
+    @Before
+    public void setUp() {
+        primeFactors = new PrimeFactors();
+    }
+
     @Test
     public void should_return_empty_list_when_n_is_one() {
-        PrimeFactors primeFactors = new PrimeFactors();
         assertThat(primeFactors.of(1), is(Lists.emptyList()));
+    }
+
+    @Test
+    public void should_return_list_2_when_n_is_two() {
+        assertThat(primeFactors.of(2), is(Arrays.asList(2)));
     }
 }
