5196db1b79f1b9cb7f15a67c3289ff665db1c637
nmkip <juan.ignacio848@gmail.com>
2018-09-22 17:03:09 -0300

    Prime factors of 3 (and any prime number)

--- src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
+++ src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
@@ -27,4 +27,9 @@ public class PrimeFactorsTest {
     public void should_return_list_2_when_n_is_two() {
         assertThat(primeFactors.of(2), is(Arrays.asList(2)));
     }
+
+    @Test
+    public void should_return_list_3_when_n_is_three() {
+        assertThat(primeFactors.of(3), is(Arrays.asList(3)));
+    }
 }
