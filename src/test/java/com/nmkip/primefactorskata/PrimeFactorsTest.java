45ac48deee6b82258d4fdd2c18d2433e97a8b17a
nmkip <juan.ignacio848@gmail.com>
2018-09-21 22:51:38 -0300

    We can calculate prime factors of 9.

--- src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
+++ src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
@@ -43,4 +43,9 @@ public class PrimeFactorsTest {
     public void should_return_list_2_2_2_when_n_is_eight() {
         assertThat(primeFactors.of(8), is(Arrays.asList(2, 2, 2)));
     }
+
+    @Test
+    public void should_return_list_3_3_when_n_is_nine() {
+        assertThat(primeFactors.of(9), is(Arrays.asList(3, 3)));
+    }
 }
