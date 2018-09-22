9c44e9b72c9d50cecddf9795ee87c08477dfea1a
nmkip <juan.ignacio848@gmail.com>
2018-09-21 22:26:34 -0300

    Removed duplication in tests.

--- src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
+++ src/test/java/com/nmkip/primefactorskata/PrimeFactorsTest.java
@@ -1,6 +1,7 @@
 package com.nmkip.primefactorskata;
 
 import org.assertj.core.util.Lists;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,15 +12,21 @@ import static org.hamcrest.Matchers.is;
 
 public class PrimeFactorsTest {
 
+    private PrimeFactors primeFactors;
+
+    @Before
+    public void setUp() throws Exception {
+        primeFactors = new PrimeFactors();
+    }
+
     @Test
     public void should_return_empty_list_when_n_is_one() {
-        PrimeFactors primeFactors = new PrimeFactors();
         assertThat(primeFactors.of(1), is(Lists.emptyList()));
     }
 
     @Test
     public void should_return_list_2_when_n_is_two() {
-        PrimeFactors primeFactors = new PrimeFactors();
         assertThat(primeFactors.of(2), is(Arrays.asList(2)));
     }
+
 }
